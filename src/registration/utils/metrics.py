a29d203ce913fef9063db179bbe635a5153a47c3
Simone Gasparini <simone.gasparini@gmail.com>
2025-10-24 23:00:12 +0200

    fix error

--- src/registration/utils/metrics.py
+++ src/registration/utils/metrics.py
@@ -34,7 +34,7 @@ def compute_rmse_between_point_clouds(
         corresponds to target.points[i]). For registration evaluation, typically
         the source would be transformed before calling this function.
     """
-    if len(source._points) != len(target.points):
+    if len(source.points) != len(target.points):
         raise ValueError(
             "Point clouds must have the same number of points to compute RMSE."
         )
