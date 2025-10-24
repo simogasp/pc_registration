658cf3ffed21662eb6a8ae56b6a0b6c4763aa243
Simone Gasparini <simone.gasparini@gmail.com>
2025-10-24 19:34:19 +0200

    fix vscode warning

--- src/registration/visualization/viewer.py
+++ src/registration/visualization/viewer.py
@@ -34,9 +34,9 @@ def draw_registration_result(
     source_temp.paint_uniform_color([1, 0.706, 0])  # yellow
     target_temp.paint_uniform_color([0, 0.651, 0.929])  # cyan
     source_temp.transform(transformation)
-    o3d.visualization.draw_geometries(       # type: ignore
+    o3d.visualization.draw_geometries(  # type: ignore
         [source_temp, target_temp], window_name=window_name
-    )  
+    )
 
 
 def print_point_cloud_info(
