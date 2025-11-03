f12e8c320bca5a8a19b5760ca8327e4d6f63916a
Simone Gasparini <simone.gasparini@gmail.com>
2025-11-03 15:36:08 +0100

    update load_and_display

--- scripts/load_and_display.py
+++ scripts/load_and_display.py
@@ -7,8 +7,13 @@ import open3d as o3d
 
 from registration.utils.logging import setup_logging
 from registration.visualization.viewer import print_point_cloud_info
+from registration.utils.transforms import (
+    get_flip_transform,
+)
+from global_registration import rough_scale_point_cloud
 
 logger = logging.getLogger(__name__)
+
 
 if __name__ == "__main__":
     # add input file argument
@@ -16,6 +21,13 @@ if __name__ == "__main__":
         description="Load and display a point cloud with its bounding boxes"
     )
     parser.add_argument("--input", type=str, help="Input file path", required=True)
+    parser.add_argument(
+        "--flip",
+        type=str,
+        choices=["x", "y", "z", "nx", "ny", "nz"],
+        help="Flip axis (x, y, or z) by 90 degrees (prefix n for negative)",
+        required=False,
+    )
     parser.add_argument(
         "-v",
         "--verbose",
@@ -31,16 +43,32 @@ if __name__ == "__main__":
 
     pcd = o3d.io.read_point_cloud(args.input)
     print_point_cloud_info(pcd, args.input)
-    # Flip it, otherwise the pointcloud will be upside down.
-    # pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
-    # print(pcd)
+
+    frame_scale = rough_scale_point_cloud(pcd)
+
+    if args.flip:
+        pcd.transform(get_flip_transform(args.flip))
+
+    # apply random transformation
+    # pcd.transform(
+    #     rototranslation_from_rotation_translation(
+    #         generate_random_rotation_matrix(), np.zeros(3)
+    #     )
+    # )
+
+    # compute and display bounding boxes
     axis_aligned_bounding_box = pcd.get_axis_aligned_bounding_box()
     axis_aligned_bounding_box.color = (1, 0, 0)
-    oriented_bounding_box = pcd.get_oriented_bounding_box()
+    oriented_bounding_box = pcd.get_minimal_oriented_bounding_box()
     oriented_bounding_box.color = (0, 1, 0)
+
     logger.info(
         "Displaying axis_aligned_bounding_box in red and oriented bounding box in green ..."
     )
-    o3d.visualization.draw(  # type: ignore
-        [pcd, axis_aligned_bounding_box, oriented_bounding_box]
+    mesh_frame_target = o3d.geometry.TriangleMesh.create_coordinate_frame(
+        size=frame_scale, origin=[0, 0, 0]
+    )
+    pcd.paint_uniform_color([1, 0.706, 0])  # yellow
+    o3d.visualization.draw_geometries(  # type: ignore
+        [pcd, axis_aligned_bounding_box, oriented_bounding_box, mesh_frame_target]
     )
